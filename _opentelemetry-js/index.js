833259c35ce0d2afe3297e9792b3ea7192a4a3cc
supanut.devops <supanut.devops@myorder.ai>
2025-01-05 22:56:54 +0700

    change exporter from consoleExporter to OTLPexporter

--- _opentelemetry-js/index.js
+++ _opentelemetry-js/index.js
@@ -1,26 +1,32 @@
 const { NodeSDK } = require("@opentelemetry/sdk-node");
+
+const {
+  OTLPTraceExporter
+} = require("@opentelemetry/exporter-trace-otlp-proto");
+
 const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
 const {
-  getNodeAutoInstrumentations,
+  getNodeAutoInstrumentations
 } = require("@opentelemetry/auto-instrumentations-node");
 
 const { Resource } = require("@opentelemetry/resources");
 const {
- SEMRESATTRS_SERVICE_NAME, SEMRESATTRS_SERVICE_VERSION
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_VERSION
 } = require("@opentelemetry/semantic-conventions");
 
 module.exports = (serviceName, serviceVersion) => {
   const sdk = new NodeSDK({
     resource: new Resource({
       [SEMRESATTRS_SERVICE_NAME]: serviceName,
-      [SEMRESATTRS_SERVICE_VERSION]: serviceVersion,
+      [SEMRESATTRS_SERVICE_VERSION]: serviceVersion
     }),
     traceExporter: new ConsoleSpanExporter(),
     instrumentations: [
       getNodeAutoInstrumentations({
-        "@opentelemetry/instrumentation-fs": { enabled: false },
-      }),
-    ],
+        "@opentelemetry/instrumentation-fs": { enabled: false }
+      })
+    ]
   });
 
   sdk.start();
